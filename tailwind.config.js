156a0ca16c8191692eb16548ecff9c3b3bd151dd
Mohammed Al-Reai <mhamad.aa1997.aa@gmail.com>
2021-06-02 12:54:32 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["/src/**.tsx","**.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+  }
+}
